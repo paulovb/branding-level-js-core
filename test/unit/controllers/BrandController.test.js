2d5d130319809228c84a3eca02e8fb3f7a8d4846
Paulo Vítor Bischof <paulo.bischof@gmail.com>
2015-07-26 14:00:57 -0300

    Implementação de testes.

--- test/unit/controllers/BrandController.test.js
+++ test/unit/controllers/BrandController.test.js
@@ -3,10 +3,18 @@ var request = require('supertest');
 describe('BrandController', function() {
 
   describe('#index()', function() {
-    it('should redirect to /brand', function (done) {
+    it('deve retornar o index da page brand', function (done) {
       request(sails.hooks.http.app)
         .get('/brand')
         .expect(200, done);
+    });
+  });
+
+  describe('#index()', function() {
+    it('deve retornar um 404 pois não existe esse registro', function (done) {
+      request(sails.hooks.http.app)
+        .get('/brand/1')
+        .expect(404, done);
     });
   });
 
